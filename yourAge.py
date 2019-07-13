abb1709f4adc13d2a6caad231e999e3ee7d95025
rekaut <rekauta@gmail.com>
2019-07-13 11:09:13 +0100

    Age calculation add to the project

---
+++ yourAge.py
@@ -0,0 +1,9 @@
+#the program in python to tell the users the year they become 80 years old.
+# as we did in input program however we add one more step to solve the algorithm as  (Nowyear - users_age)+80.
+
+ 
+name= input('Your Name:')
+age= int(input('What is your age?'))
+year_you_80= str((2019-age)+80)
+
+print('hello'+ name +'you will be 80 years old in :'+year_you_80)
