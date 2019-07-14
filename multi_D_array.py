02dc60a172dc04f0ecde273df363af51d52a53d8
rekaut <rekauta@gmail.com>
2019-07-14 14:50:57 +0100

    multi D Array/Lists imbeded inside for lops

---
+++ multi_D_array.py
@@ -0,0 +1,21 @@
+
+# multi D Lists imbeded inside for lops 
+
+###version 1 returninng in form of table and in range of soem number
+#...................................................................
+# many_grids= int(input('please input your number here '))
+# many_grids1= int(input('please input second number here'))
+# allArrays =[[0 for row in  range (many_grids)] for col in range(many_grids1)]
+# for row in range(many_grids):
+#     for col in range(many_grids1):
+#         allArrays[col][row] = row*col
+# print(allArrays)
+
+ 
+###version2
+#.....................................................................  
+staffs = [['Helgurd',44], ['Adam',35], ['Rez',18]]
+   
+for a,b in staffs:
+    print ('your name is ',a ,'your age',int(b) )
+           
