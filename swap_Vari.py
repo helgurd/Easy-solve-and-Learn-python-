b0fdee859f536bf6ac58361589824ddb2a4c3348
rekaut <rekauta@gmail.com>
2019-07-14 22:09:34 +0100

    swapping  the value of the varibles value  been made very easer with python

---
+++ swap_Vari.py
@@ -0,0 +1,12 @@
+# swapping  the value of the varibles value  been made very easer with python 
+
+
+a= 34
+b=45
+
+a,b= b,a
+
+
+print (a,b)
+ 
+# the out should give after swapping  for a:45 and for b:34 
