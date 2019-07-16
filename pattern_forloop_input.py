9ff48785f67932ac696c358267ba0575d4278940
rekaut <rekauta@gmail.com>
2019-07-16 17:36:07 +0100

    Simple code to create pattern in diffent way added

--- pattern_forloop_input.py
+++ pattern_forloop_input.py
@@ -1,4 +1,22 @@
 
+
+
+# # simple constrat of the Pattren
+# # Write a Python program to construct the following pattern, using a nested loop number.
+# # 1                                                                                                             
+# # 22                                                                                                            
+# # 333                                                                                                           
+# # 4444                                                                                                          
+# # 55555                                                                                                         
+# # 666666                                                                                                        
+# # 7777777                                                                                                       
+# # 88888888                                                                                                      
+# # 999999999
+# for i in range(10):
+#     print(str(i) * i)
+
+
+
 # Write a Python program to construct the following pattern, using a nested loop number.
 #the program should have ueser input.
 
