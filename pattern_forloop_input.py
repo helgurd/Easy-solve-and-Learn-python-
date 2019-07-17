d108396b7e03bd9cea211d22642a91d4ba9471b8
rekaut <rekauta@gmail.com>
2019-07-17 15:23:26 +0100

    comment added to line 34,  and patteren un commentd

--- pattern_forloop_input.py
+++ pattern_forloop_input.py
@@ -24,17 +24,15 @@
 # # * * * 
 # # * * * * 
 # # * * * * * 
-# # * * * * 
-# # * * * 
-# # * * 
-# # *
 
-# n = int(input("Enter Number "))
-# print('you printed', n, 'stars')
-# for i in range(n):
-#     for j in range(i):
-#         print ('*', end="")
-#     print('\n')
+
+n = int(input("Enter Number "))
+#n=10  ##you can uncomment this line and comment the input by user to get a fixed pattern. 
+print('you printed', n, 'stars')
+for i in range(n):
+    for j in range(i):
+        print ('*', end="") # you can remove * and input anythin you would like to be printed, for numbers you have to replace the * with (n) without quats just like this (n, end="")  
+    print('\n')
 
 
 
